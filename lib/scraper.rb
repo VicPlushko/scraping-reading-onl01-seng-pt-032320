232d55fb4088792f3c5cc546ef83eac3d0fc58c2
Victor Plushko <vicplushko33@gmail.com>
2020-04-27 03:27:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require 'open-uri'
 html = Nokogiri::HTML(open("https://flatironschool.com/"))
 
 doc = Nokogiri::HTML(html)
-doc.css("2a778efd-1685-5ec6-9e5a-0843d6a88b7b.inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G").text
+doc.css("2a778efd-1685-5ec6-9e5a-0843d6a88b7b.inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")[0].name
 
 puts doc.css
 
