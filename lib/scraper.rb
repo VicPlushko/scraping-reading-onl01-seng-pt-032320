681988468947d2f558a1404a2a32303cf4791a22
Victor Plushko <vicplushko33@gmail.com>
2020-04-27 03:22:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,3 +9,9 @@ doc = Nokogiri::HTML(html)
 doc.css("2a778efd-1685-5ec6-9e5a-0843d6a88b7b.inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G").text
 
 puts doc.css
+
+courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+ 
+courses.each do |course|
+  puts course.text.strip
+end
